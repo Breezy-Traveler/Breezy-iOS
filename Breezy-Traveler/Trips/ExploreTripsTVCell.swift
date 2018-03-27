1684679e4e6fda3f8de6a9200dd2b791118cbe0b
Phyllis <wong.phyllism@gmail.com>
2018-03-26 17:51:50 -0700

    Add Profile stackView add tableView add prototype cell for the side scroll collectionView

---
+++ Breezy-Traveler/Trips/ExploreTripsTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreTripsTVCell.swift
+//  Breezy-Traveler
+//
+//  Created by Phyllis Wong on 3/26/18.
+//  Copyright © 2018 Phyllis Wong. All rights reserved.
+//
+
+import UIKit
+
+class ExploreTripsTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
