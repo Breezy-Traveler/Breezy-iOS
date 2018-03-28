2a1159f591d01bc6758f6c35841868afb8cd2390
Phyllis <wong.phyllism@gmail.com>
2018-03-28 15:51:30 -0700

    add user model

---
+++ Breezy-Traveler/UserModel.swift
@@ -0,0 +1,19 @@
+//
+//  UserModel.swift
+//  Breezy-Traveler
+//
+//  Created by Phyllis Wong on 3/28/18.
+//  Copyright © 2018 Phyllis Wong. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    
+    let id: Int
+    let name: String
+    let username: String
+    let password: String
+    let email: String
+    let token: String
+}
