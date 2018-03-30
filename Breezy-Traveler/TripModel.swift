53d436d3cec4031ca1eb3d01e0bff71ec097ca85
Phyllis <wong.phyllism@gmail.com>
2018-03-30 15:31:02 -0700

    Update swift model to camelCase, change JSON from snake case

--- Breezy-Traveler/TripModel.swift
+++ Breezy-Traveler/TripModel.swift
@@ -10,13 +10,19 @@ import Foundation
 
 struct Trip: Codable {
     let place: String
-    let start_date: Date?
-    let end_date: Date?
+    let startDate: Date?
+    let endDate: Date?
     
     // FIXME: Add these type later once we get this working
 //    let hotels: [Hotel]
 //    let sites: [Site]
-    let is_public: Bool
+    let isPublic: Bool
     
+    enum CodingKeys: String, CodingKey {
+        case place
+        case startDate = "start_date"
+        case endDate = "end_date"
+        case isPublic = "is_public"
+    }
     
 }
