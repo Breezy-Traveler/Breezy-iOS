3aea06a59bfe5ec94067c3c254fa5f2d63ff6719
Phyllis <wong.phyllism@gmail.com>
2018-03-30 15:43:42 -0700

    Update trip model with Hotel property: array of Hotel types

--- Breezy-Traveler/TripModel.swift
+++ Breezy-Traveler/TripModel.swift
@@ -14,7 +14,7 @@ struct Trip: Codable {
     let endDate: Date?
     
     // FIXME: Add these type later once we get this working
-//    let hotels: [Hotel]
+    let hotels: [Hotel]
 //    let sites: [Site]
     let isPublic: Bool
     
@@ -22,7 +22,35 @@ struct Trip: Codable {
         case place
         case startDate = "start_date"
         case endDate = "end_date"
+        case hotels
         case isPublic = "is_public"
     }
+}
+
+struct Hotel: Codable {
+    let name: String
+    let address: String?
+    let visited: Bool
+    let notes: String?
+    let rating: Like?
+//
+    enum Like: Int, Codable {
+        // Zero is dislike, one is like
+        case dislike = 0
+        case like = 1
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "title"
+        case address
+        case visited = "is_visited"
+        case notes
+        case rating = "ratings"
+    }
+}
+
+struct Site {
     
 }
+
+
