87a54d7a51d3fa30cb55808827e6fb2e629a8cf6
Phyllis <wong.phyllism@gmail.com>
2018-04-04 17:41:28 -0700

    Fixed merge conflict from MyTripsCpntroller

--- Breezy-Traveler/MyTripsViewController.swift
+++ Breezy-Traveler/MyTripsViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class MyTripsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tripsTableView: UITableView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
     
     var trips = [BTTrip]()
     
@@ -20,7 +22,11 @@ class MyTripsViewController: UIViewController, UITableViewDelegate, UITableViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         tripsTableView.delegate = self
-        tripsTableView.dataSource = self        
+        tripsTableView.dataSource = self
+        usernameLabel.text = currentUser.username
+        emailLabel.text = currentUser.email
+        
+        self.hideKeyboard()
     }
     
     // FIXME: Uncomment this code when I have a user stored in keychain
