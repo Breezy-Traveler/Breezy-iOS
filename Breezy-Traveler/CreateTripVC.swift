03e644db1a70a903483a13d94bfc30f5310e8480
Phyllis <wong.phyllism@gmail.com>
2018-04-10 13:07:03 -0700

    Refactored Create Trip to segue to Trip Detail View Controller

--- Breezy-Traveler/CreateTripVC.swift
+++ Breezy-Traveler/CreateTripVC.swift
@@ -10,33 +10,36 @@ import UIKit
 
 class CreateTripVC: UIViewController {
 
+    // Instance of the Moya network stack
     let networkStack = NetworkStack()
     @IBOutlet weak var placeTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Performed when user taps outside of textfield
         self.hideKeyboard()
     }
     
-    
-    @IBAction func saveTrip(_ sender: UIBarButtonItem) {
-        guard let place = placeTextField.text else {
-            return
-        }
+    @IBAction func pressedSave(_ sender: UIBarButtonItem) {
+        guard let place = placeTextField.text else { return }
         
         let trip = BTTrip(place: place, isPublic: false)
-        
-        
         networkStack.createTrip(trip: trip) { (result) in
             switch result {
                 
             case .success(let trip):
                 print(trip)
                 
-                // Go back to My Trips View Controller
+                // FIXME: Navigate to Trip Detail View
+                
+                let tripStoryboard : UIStoryboard = UIStoryboard(name: "Trips", bundle:nil)
+                let vc = tripStoryboard.instantiateViewController(withIdentifier: "TripDetailViewController") as! TripDetailedViewController
+                
+                // Bring back to the main thread before presenting the Trip Detail View
                 DispatchQueue.main.async {
-                    self.navigationController?.popViewController(animated: true)
+                    self.present(vc, animated: true, completion: nil)
                 }
                 
             case .failure(let tripsErrors):
@@ -44,17 +47,4 @@ class CreateTripVC: UIViewController {
             }
         }
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
