aa79c40d9ab0da2bc558081ee3c93d1e94cb9a5b
LinnierGames <esericksanc@gmail.com>
2018-11-08 16:33:56 -0800

    Create User Object
    this maps to the new user

--- Breezy-Traveler/UserModel.swift
+++ Breezy-Traveler/UserModel.swift
@@ -8,6 +8,75 @@
 
 import UIKit
 
+struct User: Codable {
+    
+    // MARK: - VARS
+    
+    let id: String
+    
+    struct LocalProvider: Codable {
+        var token: String
+        var email: String
+        var username: String
+    }
+    var local: LocalProvider?
+    
+    struct Facebook: Codable {
+        var id: String
+        var token: String
+        var email: String
+        var name: String
+    }
+    var facebook: Facebook?
+    
+    struct Google: Codable {
+        var id: String
+        var token: String
+        var email: String
+        var name: String
+    }
+    var google: Google?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case local
+        case facebook
+        case google
+    }
+    
+    var username: String {
+        if let localUser = self.local {
+            return localUser.username
+        } else if let facebook = self.facebook {
+            return facebook.name
+        } else if let google = self.google {
+            return google.name
+        } else {
+            assertionFailure("no user provider found")
+            
+            return "null"
+        }
+    }
+    
+    var email: String {
+        if let localUser = self.local {
+            return localUser.email
+        } else if let facebook = self.facebook {
+            return facebook.email
+        } else if let google = self.google {
+            return google.email
+        } else {
+            assertionFailure("no user provider found")
+            
+            return "null"
+        }
+    }
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - METHODS
+}
+
 struct BTUser: Codable {
     let id: Int
     let name: String
