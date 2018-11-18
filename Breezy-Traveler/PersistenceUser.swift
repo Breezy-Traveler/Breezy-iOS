b481cd997ff73fadffdbe4fda5f9af3a749dfe06
LinnierGames <esericksanc@gmail.com>
2018-11-18 15:48:44 -0800

    Arrange UserPersistence

--- Breezy-Traveler/PersistenceUser.swift
+++ Breezy-Traveler/PersistenceUser.swift
@@ -51,9 +51,6 @@ class UserPersistence {
         }
     }
     
-    private let usernameKey: String = "username"
-    private let passwordKey: String = "password"
-    private let tokenKey: String = "token"
     private let currentUserKey: String = "currentUser"
     
     private var userProfileImageURL: URL {
@@ -69,10 +66,6 @@ class UserPersistence {
         return url
     }
     
-    // MARK: - RETURN VALUES
-    
-    // MARK: - METHODS
-    
     static var currentUser: User {
         guard let user = self._currentUser else {
             fatalError("no user logged in. Please ensure this property is not invoked without ensuring a user is logged in")
@@ -86,6 +79,47 @@ class UserPersistence {
     }
     
     static private var _currentUser: User?
+    
+    // MARK: - RETURN VALUES
+    
+    //load the user from persistence and return it
+        //does user default contain USER_ALREADY_LOGGED_IN?
+        //is user in keychains present and valid?
+        //return user
+    
+        //otherwise, clear user, if stored, from keychains since user default doesn't contain key
+    private func loadUser() -> User? {
+        /**
+         since deleting the app doesn't clear keychains, we must check if the user defaults
+         contains USER_ALREADY_LOGGED_IN before we check the keychains in the event
+         the user deletes the app.
+         */
+        
+        let userDefaults = UserDefaults.standard
+        let keychains = KeychainSwift()
+        
+        guard userDefaults.bool(forKey: currentUserKey) else {
+            
+            //clear user, if stored, from keychains since user default doesn't contain key
+            self.removeUser()
+            
+            return nil
+        }
+        
+        guard
+            let userDataFromKeychains = keychains.getData(currentUserKey),
+            let user = try? JSONDecoder().decode(User.self, from: userDataFromKeychains) else {
+                
+                //invalid user, clear the keychain and remove the key from user defaults
+                self.removeUser()
+                
+                return nil
+        }
+        
+        return user
+    }
+    
+    // MARK: - METHODS
     
     //check if there is already a user persisted in user defaults
         //make a network call to validate their token?
@@ -137,43 +171,6 @@ class UserPersistence {
                 assertionFailure(error.localizedDescription)
             }
         }
-    }
-    
-    //load the user from persistence and return it
-        //does user default contain USER_ALREADY_LOGGED_IN?
-        //is user in keychains present and valid?
-        //return user
-    
-        //otherwise, clear user, if stored, from keychains since user default doesn't contain key
-    private func loadUser() -> User? {
-        /**
-         since deleting the app doesn't clear keychains, we must check if the user defaults
-         contains USER_ALREADY_LOGGED_IN before we check the keychains in the event
-         the user deletes the app.
-         */
-        
-        let userDefaults = UserDefaults.standard
-        let keychains = KeychainSwift()
-        
-        guard userDefaults.bool(forKey: currentUserKey) else {
-            
-            //clear user, if stored, from keychains since user default doesn't contain key
-            self.removeUser()
-            
-            return nil
-        }
-        
-        guard
-            let userDataFromKeychains = keychains.getData(currentUserKey),
-            let user = try? JSONDecoder().decode(User.self, from: userDataFromKeychains) else {
-                
-                //invalid user, clear the keychain and remove the key from user defaults
-                self.removeUser()
-                
-                return nil
-        }
-        
-        return user
     }
     
     //remove the user, if stored, from persistence
