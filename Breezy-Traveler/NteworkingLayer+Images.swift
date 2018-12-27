0d4b2035bf96483aef5a6ded9fa6589268463f4d
LinnierGames <esericksanc@gmail.com>
2018-12-26 19:09:13 -0800

    Refactor Images

--- Breezy-Traveler/NteworkingLayer+Images.swift
+++ Breezy-Traveler/NteworkingLayer+Images.swift
@@ -14,30 +14,9 @@ import SwiftyJSON
 extension NetworkStack {
     
     func fetchImages(searchTerm: String, callback: @escaping (Result<[URL], UserfacingErrors>) -> ()) {
-        apiService.request(.loadTenImages(searchTerm)) { (result) in
-            switch result {
-                
-            case .success(let response):
-                switch response.statusCode {
-                case 200:
-                    guard let urlArray = try? JSONDecoder().decode([URL].self, from: response.data) else {
-                        assertionFailure("JSON data not decodable")
-                        
-                        let errors = UserfacingErrors.somethingWentWrong()
-                        return callback(.failure(errors))
-                    }
-                    
-                    callback(.success(urlArray))
-                    
-                default:
-                    let errors = UserfacingErrors.serverError(message: response.data)
-                    callback(.failure(errors))
-                }
-                
-            case .failure(let err):
-                let errors = UserfacingErrors.somethingWentWrong(message: err.localizedDescription)
-                callback(.failure(errors))
-            }
-        }
+        apiService.request(
+            .loadTenImages(searchTerm),
+            completion: jsonResponse(next: callback)
+        )
     }
 }
